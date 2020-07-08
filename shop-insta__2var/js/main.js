587836de51a22798fa01c82a5a2d2214cec15cb6
artemyg0811 <55375066+artemyg0811@users.noreply.github.com>
2020-07-09 00:40:38 +0300

    Add files via upload

---
+++ shop-insta__2var/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  //прикрепляем клик по заголовкам acc-head
+	$('#accordion .accordion-link').on('click', f_acc);
+  function f_acc(){
+  //скрываем все кроме того, что должны открыть
+    $('#accordion .acc-content').not($(this).next()).slideUp(500);
+  // открываем или скрываем блок под заголовком, по которому кликнули
+    $(this).next().slideToggle(500);
+  }
+$('.accordion-link').on('click', function(e) {
+  // e.preventDefault;
+  $('.accordion-link-img').toggleClass('accordion-link-img__active');
+});
+
+$(function() {
+  var family = $('#family');
+  var problem = $('.card-problem-bg')
+  var close = $('.close')
+  var card = $('.card')
+
+  family.click(function () {
+    problem.toggleClass('card-problem-bg-active')
+    card.toggleClass('card-active')
+  })
+  close.click(function () {
+    problem.toggleClass('card-problem-bg-active')
+    card.toggleClass('card-active')
+  })
+})
+});
